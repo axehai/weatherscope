d161cb098c8116f277fd8def49febcde8ff053bd
axehai <shukla.akshaykumar@gmail.com>
2026-03-13 21:34:19 +0530

    feat(data): add StoredActiveLocationSerializer

---
+++ app/src/main/java/com/axehai/weatherscope/data/local/serializer/StoredActiveLocationSerializer.kt
@@ -0,0 +1,30 @@
+package com.axehai.weatherscope.data.local.serializer
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.axehai.weatherscope.data.local.model.StoredActiveLocation
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+
+
+object StoredActiveLocationSerializer : Serializer<StoredActiveLocation> {
+	override val defaultValue: StoredActiveLocation = StoredActiveLocation()
+
+	override suspend fun readFrom(input: InputStream): StoredActiveLocation = try {
+		Json.decodeFromString<StoredActiveLocation>(
+			input.readBytes().decodeToString()
+		)
+	} catch (e: SerializationException) {
+		throw CorruptionException("Unable to read StoredActiveLocation", e)
+	}
+
+	override suspend fun writeTo(
+		t: StoredActiveLocation, output: OutputStream
+	) {
+		output.write(
+			Json.encodeToString(t).encodeToByteArray()
+		)
+	}
+}
