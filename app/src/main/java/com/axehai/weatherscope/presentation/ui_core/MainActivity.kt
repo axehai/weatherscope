948b47a75eb791b97ee00fc033ad394358d5979f
axehai <shukla.akshaykumar@gmail.com>
2026-03-14 00:50:44 +0530

    feat: add AndroidEntryPoint annotation to MainActivity

--- app/src/main/java/com/axehai/weatherscope/presentation/ui_core/MainActivity.kt
+++ app/src/main/java/com/axehai/weatherscope/presentation/ui_core/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.axehai.weatherscope.presentation.ui_core.theme.WeatherAppDemoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
