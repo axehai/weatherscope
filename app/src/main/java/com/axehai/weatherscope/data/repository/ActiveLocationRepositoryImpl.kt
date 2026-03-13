0c57c63677a1d66d06a3b8de867fe5bf32fa8a4a
axehai <shukla.akshaykumar@gmail.com>
2026-03-13 23:13:40 +0530

    feat(data): implement ActiveLocationRepositoryImpl using DataStore

--- app/src/main/java/com/axehai/weatherscope/data/repository/ActiveLocationRepositoryImpl.kt
+++ app/src/main/java/com/axehai/weatherscope/data/repository/ActiveLocationRepositoryImpl.kt
@@ -1,19 +1,35 @@
 package com.axehai.weatherscope.data.repository
 
+import androidx.datastore.core.DataStore
+import com.axehai.weatherscope.data.local.mapper.toDomain
+import com.axehai.weatherscope.data.local.mapper.toStored
+import com.axehai.weatherscope.data.local.model.StoredActiveLocation
 import com.axehai.weatherscope.domain.model.ActiveLocation
 import com.axehai.weatherscope.domain.repository.ActiveLocationRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class ActiveLocationRepositoryImpl  : ActiveLocationRepository {
+class ActiveLocationRepositoryImpl @Inject constructor(
+	private val dataStore: DataStore<StoredActiveLocation>
+) : ActiveLocationRepository {
 	override suspend fun getActiveLocationOrNull(): ActiveLocation? {
-		TODO("Not yet implemented")
+		val stored = dataStore.data.first()
+		return if (stored.sourceId == StoredActiveLocation.SOURCE_UNINITIALIZED) null
+		else stored.toDomain()
 	}
 
-	override fun observeActiveLocation(): Flow<ActiveLocation> {
-		TODO("Not yet implemented")
-	}
+	override fun observeActiveLocation(): Flow<ActiveLocation> =
+		dataStore.data.filter { it.sourceId != StoredActiveLocation.SOURCE_UNINITIALIZED }
+			.map { it.toDomain() }.distinctUntilChanged()
+
 
 	override suspend fun setActiveLocation(location: ActiveLocation) {
-		TODO("Not yet implemented")
+		dataStore.updateData {
+			location.toStored()
+		}
 	}
 }
